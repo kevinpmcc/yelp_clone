cd20c2070e14d6b17445d440f4fbff91f5535074
NickMountjoy <nick.mountjoy98@gmail.com>
2016-04-13 16:49:08 +0100

    Added sign in/up/out

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     resources :reviews
   end
 
-  root to: "home#index"
+  root to: "restaurants#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
